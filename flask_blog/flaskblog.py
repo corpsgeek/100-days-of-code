5296a8d2d8f825bf0d362646cfd7909e1e8d6493
corpsgeek <teamcyclone8@gmail.com>
2020-12-14 08:04:16 +0100

    Created login and registration Forms via WTForms

--- flask_blog/flaskblog.py
+++ flask_blog/flaskblog.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask import render_template
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = '58cc222d9a3993951da4cc75357b3f6d'
 posts = [
     {
         'author': 'Boora',
@@ -25,6 +26,17 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Registration', form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Log in', form=form)
 
 
 if __name__ == "__main__":
